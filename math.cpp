9d29471e68b704217cd8adc53db4937daec2099c
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-11-23 18:40:44 +0300

    first commit

---
+++ math.cpp
@@ -0,0 +1,8 @@
+//
+//  math.cpp
+//  cppm-homework-06.01
+//
+//  Created by a1ex on 11/22/23.
+//
+
+#include "math.hpp"
---
+++ math.hpp
@@ -0,0 +1,13 @@
+//
+//  math.hpp
+//  cppm-homework-06.01
+//
+//  Created by a1ex on 11/22/23.
+//
+
+#ifndef math_hpp
+#define math_hpp
+
+#include <stdio.h>
+
+#endif /* math_hpp */
