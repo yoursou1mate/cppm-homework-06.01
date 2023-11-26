e4477a2811a0383c5a1e28957cf1816ed31c39d8
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-11-27 01:14:13 +0300

    06.01_First commit

--- cppm-homework-06.01/main.cpp
+++ cppm-homework-06.01/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+#include "math.hpp"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+  
+    int x;
+    int y;
+    int z;
+    Math m (x,y);
+    
+    std::cout << "Please enter first number: ";
+    std::cin >> x;
+    std::cout << '\n';
+    
+    std::cout << "Please enter second number: ";
+    std::cin >> y;
+    std::cout << '\n';
+    
+    std::cout << "Please choose your operation (1 - addition, 2 - subtraction, 3 - multiplication, 4 - divition, 5 - exponentation): ";
+    std::cin >> z;
+    std::cout << '\n';
+    
+    if (z == 1)
+    {
+        std::cout << x << " + " << y << " =  " <<  m.addition(x,y) << std::endl;
+        
+    }
+    else if (z == 2)
+    {
+        std::cout << x << " - " << y << " =  " << m.subtraction(x,y) << std::endl;
+    }
+    else if (z == 3)
+    {
+        std::cout << x << " * " << y << " =  " << m.multiplication(x,y) << std::endl;
+    }
+    else if (z == 4)
+    {
+        std::cout << x << " / " << y << " =  " << m.divition(x,y) << std::endl;
+    }
+    else if (z == 5)
+    {
+        std::cout << x << " to the degree of " << y << " =  " << m.exponentation(x,y) << std::endl;
+    };
+    
     return 0;
 }
